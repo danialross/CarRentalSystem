728e806e306bf2a6ffcfbd77fcb42c0222710420
danialross <92746365+danialross@users.noreply.github.com>
2023-08-19 11:56:30 -0400

    added returnCar
    allows customer to return a car

--- customerPage.cpp
+++ customerPage.cpp
@@ -123,5 +123,82 @@ void rent(){
 }
 
 void returnCar(){
-    
+    string line, modal;
+    bool exist = false;
+
+    //case sensitive
+
+    cout << "Enter vehicle to return (Model Make)." << endl;
+    cout << "Your Option : " ; 
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    getline(cin, modal);
+
+    //since 6 is the smallest car length i.e "BMW X5" input less then 6 is not valid
+    while(modal.length() < 6){
+        cout << "Invalid option, try again." << endl;
+        cout << "Your Option : ";
+        getline(cin, modal);
+    }
+
+    ofstream output("temp.txt");
+    ifstream input("carDatabase.txt");
+
+    getline(input,line);
+
+    while(line.length() != 0){
+
+        if(line.find(modal) != -1){
+
+            int amountIndex = line.find(",") + 2;
+            string amount = line.substr(amountIndex,line.length() - amountIndex);
+
+            exist = true;
+            output << modal << ", " << stoi(amount)+1 << endl;
+
+            cout << endl;
+            cout << modal << " has now been return." << endl;
+            cout << "New Value is now { " << modal << ", " << stoi(amount)+1 << " }." << endl;
+            cout << endl;
+
+        }else{
+            if(output.is_open()){
+                output << line << endl;
+            }else{
+                cout << "Error occurred when accessing item from database." << endl;
+            }
+        }
+
+        getline(input,line);
+    } 
+
+    if(!exist){
+        cout << endl;
+        cout << "Vehicle does not exist." << endl;
+        cout << endl;
+    }
+
+    output.close();
+    remove("carDatabase.txt");
+    rename("temp.txt","carDatabase.txt");
+
+    string rentAnother;
+    cout << "Would you like to return another car? (y/n)" << endl;
+        
+    cout << "Your Option : " ; cin >> rentAnother;
+    rentAnother = tolower(rentAnother[0]);
+
+    while(rentAnother != "y" && rentAnother != "n"){
+        cout << "Invalid option, select 'y' or 'n'. " << endl;
+        cout << "Your Option : " ; cin >> rentAnother;
+    }
+
+    if(rentAnother == "y"){
+        rent();
+
+    }else{
+        cout << endl;
+        cout << "Returning to main page." << endl;
+        cout << endl;
+    }
+
 }
